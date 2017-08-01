f886532d7add2324a3280d6f2d6bd10a2273da33
Dipali <dipalikatkhede111@gmail.com>
2017-08-01 14:58:04 +0530

    Assignment Day 12

--- Day 12 Assignment/Assignment1/src/RandomGenerator.java
+++ Day 12 Assignment/Assignment1/src/RandomGenerator.java
@@ -21,14 +21,14 @@ LinkedBlockingQueue<Integer> list = null;
     @Override
     public void run() {
         while(true) {
-        for (int i = 0; i < 100; i++) {
 
+            int value = (int) Math.floor(Math.random()*1000);
             try {
-                list.put(i);
+                list.put(value);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }
+
         }
     }
 }
